1e3e467bd6c4fea8adcce2307a579ae16f3f5920
AmtoJohn <amto.john@iisgalvanimi.edu.it>
2021-12-20 12:38:51 +0000

    Gioco1-2.4

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function startGame() {
     myGameArea.start();
-    myGameArea.draw(redSquare);
+    //myGameArea.draw(redSquare);
+    animatedObject.loadImages();
 }
 
 var myGameArea = {
@@ -24,10 +25,52 @@ var myGameArea = {
     {
       redSquare.y = redSquare.y + redSquare.speedY;
       redSquare.x = redSquare.x + redSquare.speedX;
-    }
+    } ,
 
-    
+    drawGameObject: function(gameObject) {
+      this.context.drawImage(
+        gameObject.image,
+        gameObject.x,
+        gameObject.y,
+        gameObject.width,
+        gameObject.height
+      )
+    }
 }
+let move;
+var animatedObject = {
+  speedX: 0,
+  speedY: 0,
+  width: 60,
+  height: 60,
+  x: 10,
+  y: 120,
+  imageList: [], //Vettore che conterrà tutte le immagini caricate
+  contaFrame: 0, //Tiene conto di quanti frame sono passati
+  actualFrame: 0, //Specifica quale frame disegnare
+
+  update: function() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+    this.contaFrame++;
+    if (this.contaFrame == 1) {
+      this.contaFrame = 0;
+      this.actualFrame = (1 + this.actualFrame) % this.imageList.length;
+      //console.log(this.actualFrame);
+      this.image = this.imageList[this.actualFrame];
+    }
+  },
+  
+  loadImages: function() {
+     for (imgPath of attacking) {
+      var img = new Image(this.width, this.height);
+      img.src = imgPath;
+      this.imageList.push(img);
+      //console.log(img);
+    }
+    this.image = this.imageList[this.actualFrame];
+  }
+};
 
 var redSquare = {
     width: 20,
@@ -40,26 +83,53 @@ var redSquare = {
   };
 
   function moveup() {
-    redSquare.speedY -= 1;
+    animatedObject.speedY -= 1;
   }
   
   function movedown() {
-    redSquare.speedY += 1;
+    animatedObject.speedY += 1;
   }
   
   function moveleft() {
-    redSquare.speedX -= 1;
+    animatedObject.speedX -= 1;
   }
   
   function moveright() {
-    redSquare.speedX += 1;
-
-
+    animatedObject.speedX += 1;
   }
+
+  function attack() {
+    move = attack;
+  }
+  function jump() {
+    move = jump;
+  }
+
+  document.addEventListener('keydown', function(event) {
+    //left
+    if(event.keyCode == 37) {
+        animatedObject.x -= 10;
+    }
+    //top
+    else if(event.keyCode == 38) {
+        animatedObject.y -= 10;
+    }
+    //right
+    else if(event.keyCode == 39) {
+        animatedObject.x += 10;
+    }
+    //bottom
+    else if(event.keyCode == 40) {
+        animatedObject.y += 10;
+    }
+});
 
   function updateGameArea() {
     myGameArea.clear();
     myGameArea.move();
-    myGameArea.draw(redSquare);
+    //myGameArea.draw(redSquare);
+    myGameArea.drawGameObject(animatedObject);
+    animatedObject.update();
   }
+  
   
---
+++ sprite.js
@@ -0,0 +1,51 @@
+var attacking = [
+    "img/Attack__000.png" ,
+    "img/Attack__001.png" ,
+    "img/Attack__002.png" ,
+    "img/Attack__003.png" ,
+    "img/Attack__004.png" ,
+    "img/Attack__005.png" ,
+    "img/Attack__006.png" ,
+    "img/Attack__007.png" ,
+    "img/Attack__008.png" ,
+    "img/Attack__009.png" 
+];
+
+var idle = [
+    "img/Idle__000.png" ,
+    "img/Idle__001.png" ,
+    "img/Idle__002.png" ,
+    "img/Idle__003.png" ,
+    "img/Idle__004.png" ,
+    "img/Idle__005.png" ,
+    "img/Idle__006.png" ,
+    "img/Idle__007.png" ,
+    "img/Idle__008.png" ,
+    "img/Idle__009.png" 
+]
+
+var running = [
+    "img/Run__000.png" ,
+    "img/Run__001.png" ,
+    "img/Run__002.png" ,
+    "img/Run__003.png" ,
+    "img/Run__004.png" ,
+    "img/Run__005.png" ,
+    "img/Run__006.png" ,
+    "img/Run__007.png" ,
+    "img/Run__008.png" ,
+    "img/Run__009.png" 
+]
+var jump = [
+    "img/Jump__000.png" ,
+    "img/Jump__001.png" ,
+    "img/Jump__002.png" ,
+    "img/Jump__003.png" ,
+    "img/Jump__004.png" ,
+    "img/Jump__005.png" ,
+    "img/Jump__006.png" ,
+    "img/Jump__007.png" ,
+    "img/Jump__008.png" ,
+    "img/Jump__009.png" 
+]
+
